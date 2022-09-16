393606e81f00ea8761c9bbe6269875186628f817
mosweubarona <mosweubarona@gmail.com>
2022-09-16 13:03:18 -0700

    my 2nd commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  name     => 'puppet-lint',
-  provider => 'gem'
+install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3'
 }
